31466d5bc6799a39585367f433abf9dc9a19009b
SergioTales1 <algumemailai3@gmail.com>
2024-07-26 13:07:57 -0300

    form logic

--- blogapp/app.js
+++ blogapp/app.js
@@ -1,40 +1,62 @@
 // Carregando Módulos
-const express = require('express');
-const { engine } = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const { engine } = require("express-handlebars");
+const bodyParser = require("body-parser");
 const app = express();
-const admin = require('./routes/admin');
-const path = require('path');
-const mongoose = require('mongoose');
+const admin = require("./routes/admin");
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const flash = require("connect-flash");
 
 // Configurações
+// Sessão
+app.use(
+  session({
+    secret: "cursodenode",
+    resave: true,
+    saveUninitialized: true,
+  }),
+);
+
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  next();
+});
+//
+
+// Middleware
+
 // Body Parser
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Handlebars
 app.engine(
-  'handlebars',
+  "handlebars",
   engine({
-    defaultLayout: 'main',
+    defaultLayout: "main",
     runtimeOptions: {
       allowProtoMethodsByDefault: true,
       allowProtoPropertiesByDefault: true,
     },
-  })
+  }),
 );
-app.set('view engine', 'handlebars');
+app.set("view engine", "handlebars");
 
 // Mongoose
 
 const mongoConnection = async () => {
   try {
     mongoose.Promise = global.Promise;
-    await mongoose.connect('mongodb://127.0.0.1/blogapp');
-    console.log('Conectado ao banco de dados com sucesso!');
+    await mongoose.connect("mongodb://127.0.0.1/blogapp");
+    console.log("Conectado ao banco de dados com sucesso!");
   } catch (error) {
     console.log(
-      'Ocorreu um erro ao conectar-se com o banco de dados: ' + error
+      "Ocorreu um erro ao conectar-se com o banco de dados: " + error,
     );
   }
 };
@@ -42,13 +64,13 @@ const mongoConnection = async () => {
 mongoConnection();
 
 // Public
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Rotas
-app.use('/admin', admin);
+app.use("/admin", admin);
 
 // Outros
 const PORT = 8081;
 app.listen(PORT, () => {
-  console.log('Servidor Rodando...');
+  console.log("Servidor Rodando...");
 });
--- blogapp/routes/admin.js
+++ blogapp/routes/admin.js
@@ -1,20 +1,67 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+require("../models/Categoria");
+const Categoria = mongoose.model("categorias");
 
-router.get('/', (req, res) => {
-  res.render('admin/index');
+router.get("/", (req, res) => {
+  res.render("admin/index");
 });
 
-router.get('/posts', (req, res) => {
-  res.send('Página de posts');
+router.get("/posts", (req, res) => {
+  res.send("Página de posts");
 });
 
-router.get('/categorias', (req, res) => {
-  res.render('admin/categorias');
+router.get("/categorias", (req, res) => {
+  res.render("admin/categorias");
 });
 
-router.get('/categorias/add', (req, res) => {
-  res.render('admin/addcategorias');
+router.get("/categorias/add", (req, res) => {
+  res.render("admin/addcategorias");
+});
+
+router.post("/categorias/nova", async (req, res) => {
+  let erros = [];
+
+  if (
+    !req.body.nome ||
+    typeof req.body.nome == undefined ||
+    req.body.nome == null
+  ) {
+    console.log(req.body.nome);
+    erros.push({ texto: "Nome inválido" });
+  }
+
+  if (
+    !req.body.slug ||
+    typeof req.body.slug == undefined ||
+    req.body.slug == null
+  ) {
+    erros.push({ texto: "Slug inválido" });
+  }
+
+  if (req.body.nome.length < 2) {
+    erros.push({ texto: "Nome da categoria é muito pequeno" });
+  }
+
+  if (erros.length > 0) {
+    console.log(req.body.nome);
+    res.render("admin/addcategorias", { erros });
+  } else {
+    const novaCategoria = {
+      nome: req.body.nome,
+      slug: req.body.slug,
+    };
+
+    try {
+      await new Categoria(novaCategoria).save();
+      req.flash("success_msg", "Categoria salva com sucesso!");
+      res.redirect("/admin/categorias");
+    } catch (error) {
+      req.flash("error_msg", `Error ao salvar categoria ${error}`);
+      res.redirect("/admin");
+    }
+  }
 });
 
 module.exports = router;
